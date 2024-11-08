817e1864a03a7bc928e304bbed94a4859e3b34b4
TomateLevru <podlmww@gmail.com>
2024-11-08 12:50:38 +0000

    Add files via upload

---
+++ 1a50vera-ex2.cs
@@ -0,0 +1,19 @@
+﻿    using System;
+    // exercio 2
+    public class umA50
+    {
+        public static void Main(string[] args)
+        {
+            int soma = 0;
+            for (int i = 0; i <= 50; i++)
+            {
+                if (i % 2 != 0)
+                {
+
+                    soma = soma + i;
+               
+                }
+            }
+            Console.WriteLine("total de nº impares = " + soma);
+        }
+    }
---
+++ uma20vera.cs
@@ -0,0 +1,17 @@
+﻿// exercio 1
+using System;
+public class num1a20
+{
+
+    public static void Main(string[] args)
+    {
+        for (int i = 0; i <= 20; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+}
